fcddb1a91b683f1d39978bfb3c6110aeaf5c5a0c
johnalexf <johnalexfr@gmail.com>
2026-05-11 10:55:54 -0500

    feat: ingresando dos primeros registros en libreta

--- GA6-220501096-AA2-EV01_SQL_Libreta/script_libreta.sql
+++ GA6-220501096-AA2-EV01_SQL_Libreta/script_libreta.sql
@@ -17,3 +17,9 @@ SHOW TABLES;
 
 --Ver la estructura de la tabla libreta
 DESCRIBE libreta;
+
+-- Ingreso de registros especificados en el taller
+INSERT INTO libreta(nombre_libreta, domicilio_libreta, telefono_libreta) 
+VALUES
+    ('Alberto Mores' , 'Colon 123' , '4234567'),
+    ('Juan Torres' , 'Avellaneda 135' , '4458787');
