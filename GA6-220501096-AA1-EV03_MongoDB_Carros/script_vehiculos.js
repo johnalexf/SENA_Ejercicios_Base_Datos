cb552e1e680bb7aa49574b3b3573e2344fddd981
johnalexf <johnalexfr@gmail.com>
2026-05-12 10:58:44 -0500

    feat: inicializar script_vehiculo con instrucciones de conexion a mongosh

---
+++ GA6-220501096-AA1-EV03_MongoDB_Carros/script_vehiculos.js
@@ -0,0 +1,8 @@
+// Los siguientes comandos son para ejecutarlos desde powershell
+// Es de aclarar que mongoDB debio ser previamente instalado y configurado
+// En el siguiente video se encuentra un pequeño tutorial donde explican la instalacion y uso de mongoDB
+// https://www.youtube.com/watch?v=4YnfILQSfYI autor https://www.youtube.com/@programadornovato
+
+// Conectarse a mongoDB
+mongosh
+
